26190c8a5545804591118aac1b4ea8ab045a2855
Lucas Felipe Costa <lucasfelipecost4@gmail.com>
2021-02-23 22:51:18 -0300

    Primeira execucao com elixir

--- lib/rocketpay_lfc_web/controllers/welcome_controller.ex
+++ lib/rocketpay_lfc_web/controllers/welcome_controller.ex
@@ -1,7 +1,23 @@
 defmodule RocketpayLfcWeb.WelcomeController do
   use RocketpayLfcWeb, :controller
 
-  def index(conn,_params) do
-    text(conn, "Welcome to the Rockerplay lfc API")
+  alias RocketpayLfc.Number
+
+  def index(conn, %{"filename" => file}) do
+    file
+    |> Number.sum_from_file()
+    |> handle_response(conn)
+  end
+
+  defp handle_response({:ok, %{result: result}}, conn) do
+    conn
+    |> put_status(:ok)
+    |> json(%{message: "Here is your number #{result}"})
+  end
+
+  defp handle_response({:error, result}, conn) do
+    conn
+    |> put_status(:bad_request)
+    |> json(result)
   end
 end
